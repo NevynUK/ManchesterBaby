ff35c627ff4afaf3ad1d38a2e2eb8eb26e8f9924
Mark Stevens <NevynUK@users.noreply.github.com>
2021-03-21 14:56:26 +0000

    Corrected spelling error in comment.

--- Python Emulation/CPU.py
+++ Python Emulation/CPU.py
@@ -6,7 +6,7 @@ import Register
 import StoreLines
 
 #
-#   Dictionary containing the instruction set, the associted 'mnemonic' and the 'description' of the instruction.
+#   Dictionary containing the instruction set, the associated 'mnemonic' and the 'description' of the instruction.
 #
 #   Instructions are stored in a list with each entry being a dictionary item.  The dictionary item contains the
 #   SSEM opcode for instruction along with information about the instruction:
