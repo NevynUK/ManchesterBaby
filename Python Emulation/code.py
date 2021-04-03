3638f62c019057ca7ea7d1fe501bab554004542f
Mark Stevens <mark@thepcsite.co.uk>
2021-04-03 17:24:31 +0100

    Added run / stop indicator LED

--- Python Emulation/code.py
+++ Python Emulation/code.py
@@ -26,21 +26,28 @@ lcd = character_lcd.Character_LCD_SPI(spi, latch, lcd_columns, lcd_rows)
 
 lcd.message = '  Manchester Baby\n\n     Pico 2040'
 
-test = DigitalInOut(board.GP17)
-test.direction = Direction.INPUT
-test.pull = Pull.DOWN
+runLed = DigitalInOut(board.GP27)
+runLed.direction = Direction.OUTPUT
+runLed.value = True
+
+stopLed = DigitalInOut(board.GP26)
+stopLed.direction = Direction.OUTPUT
+stopLed.value = False
 
 displayTube = RGBMatrixDisplayTube()
 baby = ManchesterBaby()
 baby.Assembler('Samples/hfr989.asm')
 baby.Print()
 start = time.monotonic()
+stopLed.value = True
+runLed.value = False
 message ='  Manchester Baby\n\nExec: hfr989.asm'
 lcd.clear()
 lcd.message = message
 instructionCount = baby.RunProgram(debugging = False, progress = ProgressUpdate, updateDisplayTube  = displayTube.Show)
+runLed.value = True
+stopLed.value = False
 end = time.monotonic()
-#print('Elapsed time {}'.format(end - start))
 
 message ='  Manchester Baby\nExec: hfr989.asm\nLines: {}\nTime: {} sec'.format(instructionCount, end - start)
 lcd.clear()
