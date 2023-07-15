7c9f6fb1db02c77a6d7b3f363b2e81715be58ff7
Mark Stevens <mark@thepcsite.co.uk>
2023-07-15 20:35:32 +0100

    Minor tidy up.

--- Source/CPP/Desktop/ssem_main.cxx
+++ Source/CPP/Desktop/ssem_main.cxx
@@ -13,9 +13,8 @@ extern "C" int execute_unit_tests();
 extern "C" int main(int argc, char *argv[])
 {
 #ifdef UNIT_TESTS
-    return(execute_unit_tests() ? 0 : -1);    
+    return(execute_unit_tests() ? 0 : -1);
 #else
-
     Instructions::PopulateLookupTable();
     StoreLines *storeLines = Compiler::Compile("hfr989.ssem");
     ConsoleUserInterface consoleUserInterface;
@@ -30,6 +29,7 @@ extern "C" int main(int argc, char *argv[])
         instructionCount++;
     }
 
+    printf("\n\n\nProgram execution complete.\n");
     consoleUserInterface.UpdateDisplayTube(*storeLines);
     printf("Executed %d instructions.\n", instructionCount);
 #endif
